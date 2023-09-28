cb1f347f0246515d564a9b07befcde6df5f67199
LUIS-GG10 <Luis_engg@hotmail.comgit config --global user.email Luis_engg@hotmail.com>
2023-09-28 17:30:00 -0600

    first

---
+++ JS/Index.js
@@ -0,0 +1,12 @@
+function Newfuncion(){
+console.log('nueva funcion')
+}
+
+function PerimetroCuadrado(lado){
+    let Perimetro=lado*4
+    console.log(` El perimetro es ${Perimetro}`)
+}
+
+
+const newflecha=(P)=>{console.log('hola'+ P)}
+const Perimetro=(lado)=>Per=lado*4
